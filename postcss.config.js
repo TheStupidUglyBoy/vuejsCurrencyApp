b7a04f083699f09bbb47d60de945a48f4608e712
fortitudinous <fortitudinous@126.com>
2020-10-23 13:46:41 +0800

    commit here

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,20 @@
+// module.exports = {
+//     plugins: [
+//       // ...
+//       require('tailwindcss'),
+//       require('autoprefixer'),
+//       // ...
+//     ]
+//   }
+
+  const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
 module.exports = {
     plugins: [
-      // ...
-      require('tailwindcss'),
-      require('autoprefixer'),
-      // ...
+        tailwindcss("./tailwind.config.js"),
+        autoprefixer({
+            add: true,
+            grid: true
+        }),
     ]
-  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+
+  	'./src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+
+  ],
   theme: {
     extend: {},
   },
